ac1d159634721352bb5c335872411aad69afd006
sunmini11 <yaifirst_sunmini@hotmail.com>
2016-11-03 12:28:28 +0700

    gvhiku

--- app/src/main/java/com/egco428/xmlparser/MainActivity.java
+++ app/src/main/java/com/egco428/xmlparser/MainActivity.java
@@ -5,36 +5,40 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
-    private String url1 = "http://api.openweathermap.org/data/2.5/weather?q=";
-    private String url2 = "&mode=xml&appid=b994c35873c281b5257ad8edbf3c6dfb";
-    private EditText location,country,temperature,humidity,pressure;
-    private HandleXML obj;
+    private EditText display,url;
+    private JsonParser obj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        location = (EditText)findViewById(R.id.locInput);
-        country = (EditText)findViewById(R.id.countryTxt);
-        temperature = (EditText)findViewById(R.id.tempTxt);
-        humidity = (EditText)findViewById(R.id.humidTxt);
-        pressure = (EditText)findViewById(R.id.pressTxt);
+        display = (EditText)findViewById(R.id.displayTxt);
+        url = (EditText)findViewById(R.id.urlTxt);
+
+
+
     }
 
     public void openWeatherMethod(View view){
         String url = location.getText().toString();
-        String finalUrl = url1 + url + url2;
 
-        obj = new HandleXML(finalUrl);
-        obj.fetchXML();
+        obj = new JsonParser(finalUrl);
+        obj.fetchJSON();
         while (obj.parsingComplete);
         country.setText(obj.getCountry());
-        temperature.setText(obj.getTemperature());
+
         humidity.setText(obj.getHumidity());
         pressure.setText(obj.getPressure());
 
 
+        float kelvin = Float.parseFloat(obj.getTemperature());
+        float cel = kelvin-273;
+        DecimalFormat format = new DecimalFormat("0.00");
+        temperature.setText(String.valueOf(format.format(cel)));
     }
 
 }
